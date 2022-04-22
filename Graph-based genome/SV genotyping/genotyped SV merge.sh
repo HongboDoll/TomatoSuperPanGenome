29e571da57d83a0dfb83fec4220c65abe84306a4
HongboDoll <101861734+HongboDoll@users.noreply.github.com>
2022-04-22 16:27:23 +0800

    Update genotyped SV merge.sh

--- Graph-based genome/SV genotyping/genotyped SV merge.sh
+++ Graph-based genome/SV genotyping/genotyped SV merge.sh
@@ -19,7 +19,7 @@ do
 done
 	
 VCFS=`ls vcf/*.filter.norm.vcf.gz`
-bcftools merge -0 $VCFS | bcftools norm -m -any -N -c x --fasta-ref $ref > 321_acc.merge.vcf
+bcftools merge -0 $VCFS > 321_acc.merge.vcf
 
 grep -v '#' 321_acc.merge.vcf | grep -v 'HiC_scaffold' | sort -k1,1 -k2,2n > t
 cat <(grep '#' 321_SV.merge.vcf | grep -v HiC_scaffold) <(paste <(cut -f '1-2' t) <(awk 'BEGIN{n=1}{print "SV"n;n+=1}' t) <(cut -f '4-999' t)) > tt  && mv tt 321_SV.merge.vcf
